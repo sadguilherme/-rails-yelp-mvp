83ab3e430244ce1c42be5411f70fe5c15d96fe0d
Guilherme Sad <sadguilherme@gmail.com>
2020-11-18 15:34:43 -0300

    dsojfdgsojgfojgfjo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # get "restaurants", to: "restaurants#index"
+
+  #  get "restaurants/new", to: "restaurants#new"
+
+  #   get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
+  #    post "restaurants", to: "restaurants#create"
+
+  #    get " restaurants:id/edit", to: "restaurants#edit"
+
+
+  #   patch "restaurants/:id", to: "restaurants#update"
+
+  #   delete "restaurants/:id", to: "restaurants#destroy"
+
+
+
+  #  get 'reviews/new'
+   resources :restaurants do
+     resources :reviews, only: %i[new create]
+
+
+  end
+
+  #resources :reviews, only: :destroy
+
+
+  # get 'top', to: 'restaurants#top'
+
 end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Restaurant.create!(name: Faker::Restaurant.name,
+                     address: Faker::Address.full_address,
+                     phone: Faker::Restaurant.phone,
+                     category: Faker::Restaurant.category,
+                     rating: rand(0..5))
+end
